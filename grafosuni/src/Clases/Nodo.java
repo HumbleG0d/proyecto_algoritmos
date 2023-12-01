a7052b0c8a97b0e9bd3f12114581156936a7660f
HumbleG0d <sergiouni2000@gmail.com>
2023-12-01 17:46:45 -0500

    Add project

---
+++ grafosuni/src/Clases/Nodo.java
@@ -0,0 +1,64 @@
+package Clases;
+
+/**
+ *
+ * @author Jose H Elel
+ */
+public class Nodo {    
+   private  int nombre ;
+   private boolean visitado ;
+   private boolean etiqueta;
+   private double acumulado; // lleva el acoulado de cada nodo
+   private Nodo Predecesor;
+   
+   public Nodo(){
+       this.nombre =-1;
+       this.visitado = false;
+       this.etiqueta = false;
+       this.Predecesor  = null;
+       this.acumulado =0;       
+
+   }
+
+     public int getNombre() {
+        return nombre;
+    }
+
+    public boolean isVisitado() {
+        return visitado;
+    }
+
+    public boolean isEtiqueta() {
+        return etiqueta;
+    }
+
+    public double getAcumulado() {
+        return acumulado;
+    }
+
+    public Nodo getPredecesor() {
+        return Predecesor;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setVisitado(boolean visitado) {
+        this.visitado = visitado;
+    }
+
+    public void setEtiqueta(boolean etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public void setAcumulado(double acomulado) {
+        this.acumulado = acomulado;
+    }
+
+    public void setPredecesor(Nodo Predecesor) {
+        this.Predecesor = Predecesor;
+    }
+
+    
+}
